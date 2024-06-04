16abce6afc5195e7e9e80afba5de66ef3eb54a6e
Fadi abji <fadi.abji@hotmail.com>
2024-06-04 21:21:51 +0200

    Add project files.

---
+++ ReflectionDemo.EntityFramework/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Text;
+
+namespace ReflectionDemo.EntityFramework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static void CrateSQLTable(Type type, string tableName)
+        {
+            //Create a connection
+            var connection = new SqlConnection("Server=.\\SQlExpress;Database=ReflectionDemoDb;Integerated");
+
+            // construct the query
+            // 1 Get the properties
+
+            var properties = type.GetProperties();
+
+            var queryBuilder = new StringBuilder();
+
+            queryBuilder.Append($"CREATE TABLE {tableName} (");
+
+            foreach( var property in properties ) 
+            {
+                // check if the propery is primaray key
+                if (property.Name == "id" && property.PropertyType.Name == "Int32")// Auto Incrementd Primery key
+                    queryBuilder.Append("Id");
+            }
+
+        }
+    }
+}
---
+++ ReflectionDemo/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+
+namespace ReflectionDemo
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var person = new Person();
+
+            //Type PersonType = person.GetType();
+
+            Type personType = typeof(Person);
+
+            var properties = personType.GetProperties();
+
+            foreach (var property in properties)
+            {
+                Console.WriteLine($"Proterty: Type: {property.PropertyType.Name} | Name : {property.Name}");
+            }
+
+            Person person = new();
+
+            var methods = personType.GetMethods();
+
+            foreach (var method in methods)
+            {
+                Console.WriteLine($"Method : {method.Name} | Return Type: {method.ReturnType}");
+                if (method.Name == "Pirnt")
+                {
+                    method.Invoke(person, null);
+                }
+            }
+
+            static void AttributeTest(Type type)
+            {
+                // Get all methods 
+                var allMethods = type.GetMethods();
+                var methodsWithAttribute = allMethods.Where(m => m.GetCustomAttribute(typeof(MethodForRunAttribute)) != null);
+
+                var obj = Activator.CreateInstance(type);
+
+                foreach (var method in methodsWithAttribute)
+                {
+                    var attribute = (MethodForRunAttribute)method.GetCustomAttribute(typeof(MethodForRunAttribute));
+                    Console.WriteLine($"{method.Name} run for {attribute.RunCount} times");
+                    method.Invoke(obj, null);
+                    for (int i  = 0; i < attribute.RunCount; i++)
+                    {
+                        method.Invoke(obj, null);
+                    }
+                }
+
+
+            }
+
+
+            Console.WriteLine("---------------------");
+            AttributeTest(typeof(Person));
+        }
+    }
+
+    public class Person
+    {
+        //private string _firstName;
+        //public string GetFirstName() { return _firstName; } 
+        //public void SetFirstName(string firstName) { _firstName = firstName; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public int ZipCode { get; set; }
+
+        [MethodForRun(RunCount = 3)]
+        public void Pirnt()
+        {
+            Console.WriteLine($"{FirstName} {LastName}");
+        }
+
+        public void Move(int newZipCode)
+        {
+            ZipCode = newZipCode;
+            Console.WriteLine($"{FirstName} {LastName} has been moved to {newZipCode}");
+        }
+
+        [MethodForRun(RunCount = 1)]
+        public void sayHi()
+        {
+            Console.WriteLine("Hi");
+        }
+
+    }
+
+
+    public class MethodForRunAttribute : Attribute
+    {
+        public int RunCount { get; set; }
+
+    }
+}
